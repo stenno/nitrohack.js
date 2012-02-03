e9765165de850912f51de2d37a4937278df68544
stenno <nitrohack.js@app42.de>
2012-02-03 04:32:43 +0100

    [add] Socket

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+var util = require('util');
+var net = require('net');
+var Socket = net.Socket;
+
 var nitro = function() {
     this.host = 'localhost';
     this.port = 7115;
-
-      
+    Socket.call(this);
 };
 
-nitro.prototype.auth = function() {
+util.inherits(nitro, Socket);
 
+
+nitro.prototype.auth = function() {
 };
 
 module.exports = nitro;
