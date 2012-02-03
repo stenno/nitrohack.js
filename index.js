ce5f5b3916f1dac0ce4ee2b5b6d446bb1836dce3
stenno <nitrohack.js@app42.de>
2012-02-03 06:04:14 +0100

    [add] auth implemented

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ var nitro = function() {
     this.host = 'localhost';
     this.port = 7115;
     Socket.call(this);
+    this.setEncoding('utf8');
+    this.on('data') = function(data) {
+      pdata = JSON.parse(data);
+      data("");
+    }
 };
 
 util.inherits(nitro, Socket);
 
-
-nitro.prototype.auth = function() {
+nitro.prototype.auth = function(username, password) {
+  var socket = this;
+  var cmd = {"auth":{"username": username, "password": password}};
+  this.connect(this.port, this.host, function() {
+    socket.write(JSON.stringify(cmd));  
+  });
 };
 
 module.exports = nitro;
